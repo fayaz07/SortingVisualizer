f60cbef183f27c5a97569a944bf00545c6caffdf
Mohammad Fayaz <fayaz.mohammad@thoughtworks.com>
2022-12-09 16:05:48 +0530

    init sorting screen

--- app/src/main/java/dev/mohammadfayaz/sorting/ui/home/HomeScreen.kt
+++ app/src/main/java/dev/mohammadfayaz/sorting/ui/home/HomeScreen.kt
@@ -1,9 +1,6 @@
 package dev.mohammadfayaz.sorting.ui.home
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
---
+++ app/src/main/java/dev/mohammadfayaz/sorting/ui/sorting/SortingScreen.kt
@@ -0,0 +1,15 @@
+package dev.mohammadfayaz.sorting.ui.sorting
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+private fun SortingScreenPreview() {
+
+}
+
+@Composable
+fun SortingScreen() {
+
+}
