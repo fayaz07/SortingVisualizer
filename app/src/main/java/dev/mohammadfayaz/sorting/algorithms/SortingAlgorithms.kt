673f57b067cc88660d16529a88d168d58791967d
Mohammad Fayaz <fayaz.mohammad@thoughtworks.com>
2022-12-10 09:22:15 +0530

    add routes for algorithms

--- app/src/main/java/dev/mohammadfayaz/sorting/algorithms/SortingAlgorithms.kt
+++ app/src/main/java/dev/mohammadfayaz/sorting/algorithms/SortingAlgorithms.kt
@@ -1,9 +1,14 @@
 package dev.mohammadfayaz.sorting.algorithms
 
-enum class SortingAlgorithms(val title: String) {
-  BUBBLE_SORT("Bubble Sort"),
-  SELECT_SORT("Selection Sort"),
-  INSERT_SORT("Insertion Sort"),
-  MERGE_SORT("Merge Sort"),
-  QUICK_SORT("Quick Sort"),
+import dev.mohammadfayaz.sorting.navigation.Routes
+
+enum class SortingAlgorithms(
+  val title: String,
+  val route: String
+) {
+  BUBBLE_SORT("Bubble Sort", Routes.bubbleSort),
+  SELECT_SORT("Selection Sort", Routes.selectionSort),
+  INSERT_SORT("Insertion Sort", Routes.insertionSort),
+  MERGE_SORT("Merge Sort", Routes.mergeSort),
+  QUICK_SORT("Quick Sort", Routes.quickSort),
 }
--- app/src/main/java/dev/mohammadfayaz/sorting/navigation/Routes.kt
+++ app/src/main/java/dev/mohammadfayaz/sorting/navigation/Routes.kt
@@ -2,4 +2,10 @@ package dev.mohammadfayaz.sorting.navigation
 
 object Routes {
   const val home = "home"
+
+  const val bubbleSort = "bubbleSort"
+  const val selectionSort = "selectionSort"
+  const val insertionSort = "insertionSort"
+  const val mergeSort = "mergeSort"
+  const val quickSort = "quickSort"
 }
