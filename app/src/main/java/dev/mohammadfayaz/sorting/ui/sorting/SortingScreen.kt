10170d53d1b25ce4590a9b2e26964f7f4e4f1c06
Mohammad Fayaz <fayaz.mohammad@thoughtworks.com>
2022-12-11 14:16:33 +0530

    update UI for sorting screen

--- app/src/main/java/dev/mohammadfayaz/sorting/ui/sorting/SortingScreen.kt
+++ app/src/main/java/dev/mohammadfayaz/sorting/ui/sorting/SortingScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.text.KeyboardOptions
@@ -19,6 +20,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -41,6 +43,11 @@ private fun SortingScreenPreview() {
 fun SortingScreen(algorithm: SortingAlgorithm) {
   val viewModel: SortingScreenViewModel = viewModel()
   val state = viewModel.state.collectAsState().value
+
+  LaunchedEffect(Unit) {
+    viewModel.setAlgorithm(algorithm)
+  }
+
   Scaffold(
     topBar = {
       TopAppBar(title = { Text(text = algorithm.title) })
@@ -62,7 +69,6 @@ private fun Body(
       .padding(paddingValues)
       .padding(all = 8.dp)
       .fillMaxSize()
-      .verticalScroll(rememberScrollState())
   ) {
     OutlinedTextField(
       modifier = Modifier
@@ -79,6 +85,8 @@ private fun Body(
     )
     Spacer(modifier = Modifier.padding(top = 8.dp))
     SpeedRadioButton(state, viewModel)
+
+    SortingVisual(state, viewModel)
   }
 }
 
@@ -91,31 +99,42 @@ private fun SpeedRadioButton(state: SortingScreenState, viewModel: SortingScreen
       style = MaterialTheme.typography.titleLarge,
       modifier = Modifier.padding(start = 8.dp)
     )
-    radioOptions.forEach { item ->
-      Row(
-        modifier = Modifier
-          .fillMaxWidth()
-          .selectable(
+    Row {
+      radioOptions.forEach { item ->
+        Row(
+          modifier = Modifier
+            .selectable(
+              selected = (item == state.speed),
+              onClick = {
+                viewModel.selectSpeed(item)
+              }
+            )
+            .padding(horizontal = 4.dp),
+          verticalAlignment = Alignment.CenterVertically
+        ) {
+          RadioButton(
             selected = (item == state.speed),
-            onClick = {
-              viewModel.selectSpeed(item)
-            }
+            onClick = { viewModel.selectSpeed(item) }
           )
-          .padding(horizontal = 16.dp),
-        verticalAlignment = Alignment.CenterVertically
-      ) {
-        RadioButton(
-          selected = (item == state.speed),
-          onClick = { viewModel.selectSpeed(item) }
-        )
-        Text(
-          text = item.name,
-          style = MaterialTheme.typography.bodyLarge.copy(
-            fontWeight = FontWeight.Bold
-          ),
-          modifier = Modifier.padding(start = 8.dp),
-        )
+          Text(
+            text = item.name,
+            style = MaterialTheme.typography.bodyLarge.copy(
+              fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier.padding(start = 4.dp),
+          )
+        }
       }
     }
+  }
+}
+
+@Composable
+private fun SortingVisual(
+  state: SortingScreenState,
+  viewModel: SortingScreenViewModel
+) {
+  LazyColumn {
+
   }
 }
